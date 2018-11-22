6caef1e4bf62b43b9fe3ffe3724e758e228d0278
Aleksey Boikovskyi <aleksey.boikovskyi@trustsourcing.com>
2018-11-23 00:17:42 +0200

    new migration

---
+++ PopCorn.DataLayer/Migrations/20181122221640_Changes5.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PopCorn.DataLayer.Migrations
+{
+    public partial class Changes5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Income",
+                table: "FinanceTypes",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Income",
+                table: "FinanceTypes");
+        }
+    }
+}
--- PopCorn.DataLayer/Models/FinanceType.cs
+++ PopCorn.DataLayer/Models/FinanceType.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using PopCorn.DataLayer.Attributes;
 using PopCorn.DataLayer.Enums;
 using PopCorn.DataLayer.Models.Interfaces;
@@ -13,8 +14,13 @@ namespace PopCorn.DataLayer.Models
 		[TableView(Name = "Название")]
 		[InputView(Name = "Название")]
 		public string Name { get; set; }
-
+		
+		[InputView(Name = "Доход")]
 		public bool Income { get; set; }
+
+		[TableView(Name = "Доход")]
+		[NotMapped]
+		public string IncomeStr => Income ? "+" : "-";
 
 		[InputView(Name = "Описание", Type = InputFieldType.TextArea)]
 		public string Description { get; set; }
